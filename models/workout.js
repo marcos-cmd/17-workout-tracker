6aa2329dc61d3e1a950bca99b68a5fbc9da5a386
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-12-18 11:02:12 -0800

    fixed workout error

--- models/workout.js
+++ models/workout.js
@@ -1,53 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: () => new Date(),
-    },
-    exercises: [
-        {
-            type: {
-                type: String,
-                trim: true,
-                required: "Please enter a type of exercise"
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: "Please enter the name of this exercise"
-            },
-            duration: {
-                type: Number,
-                required: "Please enter an exercise duration in minutes"
-            },
-            weight: {
-                type: Number
-            },
-            sets: {
-                type: Number
-            },
-            distance: {
-                type: Number
-            }
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
         },
-    ]
-},
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Please enter an exercise duration in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    },
     {
         toJSON: {
-            //include the virtual properties at request
+            // include any virtual properties when data is requested
             virtuals: true
         }
-    });
+    }
+);
 
+// adds a dynamically-created property to schema
 workoutSchema.virtual("totalDuration").get(function () {
-    // 'reduce' array of exercises down to just the sum of their durations
+    // "reduce" array of exercises down to just the sum of their durations
     return this.exercises.reduce((total, exercise) => {
         return total + exercise.duration;
     }, 0);
 });
 
-const Workout = mongoose.model('Workout', workoutSchema);
+// Create workout collection to be exported using the defined schema
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
